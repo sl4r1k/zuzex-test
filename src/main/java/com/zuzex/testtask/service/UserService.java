c393d8a9a1b6cb1655e42e934a99a1fc398aa5e2
sl4r1k <sl4r1k@gmail.com>
2023-07-08 19:37:29 +0300

    add user service

---
+++ src/main/java/com/zuzex/testtask/service/UserService.java
@@ -0,0 +1,44 @@
+package com.zuzex.testtask.service;
+
+import com.zuzex.testtask.entity.User;
+import com.zuzex.testtask.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User add(User user) {
+        return this.repository.save(user);
+    }
+
+    public User edit(User user) {
+        if (user.getId() != null && this.repository.findById(user.getId()).isPresent()) {
+            throw new RuntimeException(
+                String.format(
+                    "User with id %s not found",
+                    user.getId()
+                )
+            );
+        }
+        return this.repository.save(user);
+    }
+
+    public void removeById(Long id) {
+        this.repository.deleteById(id);
+    }
+
+    public Optional<User> getById(Long id) {
+        return this.repository.findById(id);
+    }
+
+    public List<User> getAll() {
+        return this.repository.findAll();
+    }
+}
