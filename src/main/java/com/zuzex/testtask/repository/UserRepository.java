0bb8b96881e89795c57eea60610afc4b1da7bbb5
sl4r1k <sl4r1k@gmail.com>
2023-07-08 21:56:54 +0300

    override find all by id and add extra find by name method

--- src/main/java/com/zuzex/testtask/repository/UserRepository.java
+++ src/main/java/com/zuzex/testtask/repository/UserRepository.java
@@ -4,9 +4,16 @@ import com.zuzex.testtask.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     @Override
     List<User> findAll();
+
+    @Override
+    List<User> findAllById(Iterable<Long> ids);
+
+
+    Optional<User> findByName(String name);
 }
