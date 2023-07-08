27ebfbfca7c37cce688db09bbc4598ac35543f17
sl4r1k <sl4r1k@gmail.com>
2023-07-08 19:30:06 +0300

    add user repository

---
+++ src/main/java/com/zuzex/testtask/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.zuzex.testtask.repository;
+
+import com.zuzex.testtask.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
