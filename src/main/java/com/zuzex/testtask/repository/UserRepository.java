3730f7f31950551a6ffec5fbdc28415af5a53265
sl4r1k <sl4r1k@gmail.com>
2023-07-08 19:36:56 +0300

    override find all users method with List<T> returning type

--- src/main/java/com/zuzex/testtask/repository/UserRepository.java
+++ src/main/java/com/zuzex/testtask/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.zuzex.testtask.repository;
 import com.zuzex.testtask.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Override
+    List<User> findAll();
+}
