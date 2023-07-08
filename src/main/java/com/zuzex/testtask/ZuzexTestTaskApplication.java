5c04621be6eccbdc301f221eddccaf98704c6266
sl4r1k <sl4r1k@gmail.com>
2023-07-08 19:10:57 +0300

    Initial commit

---
+++ src/main/java/com/zuzex/testtask/ZuzexTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zuzex.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuzexTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuzexTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuzex/testtask/ZuzexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuzex.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuzexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
